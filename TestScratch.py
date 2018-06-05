089ba8349bb76bb10b8ce7f7136f890588eed8b2
Nikhil Kamath <nikhilvk91@gmail.com>
2018-06-05 12:26:26 -0300

    Add Sentiment CSV

---
+++ TestScratch.py
@@ -0,0 +1,6 @@
+from more_itertools import unique_everseen
+
+
+f = ["1", "1"]
+
+print (unique_everseen(f))
--- tweet.py
+++ tweet.py
@@ -1,9 +1,15 @@
 import tweepy
 import time
 import json
+import operator
 import csv
+import pandas as pd
+
 import re
+from more_itertools import unique_everseen
+
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 # from elasticsearch import Elasticsearch
 # from elasticsearch_dsl import connections
 # connections.create_connection(hosts=['localhost'],timeout=20)
@@ -17,54 +23,92 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
+
 def get_profile(screen_name):
-        api = tweepy.API(auth)
-        try:
-                user_profile = api.get_user(screen_name)
-                print(screen_name)
-        except tweepy.error.TweepError as e:
-                user_profile = json.loads(e.response.text)
-        return user_profile
+    api = tweepy.API(auth)
+    try:
+        user_profile = api.get_user(screen_name)
+        print(screen_name)
+    except tweepy.error.TweepError as e:
+        user_profile = json.loads(e.response.text)
+    return user_profile
+
+
 def get_trends(location_id):
-        api = tweepy.API(auth)
-        try:
-                trends = api.trends_place(location_id)
-        except tweepy.error.TweepError as e:
-                trends = json.loads(e.response.text)
-        return trends
+    api = tweepy.API(auth)
+    try:
+        trends = api.trends_place(location_id)
+    except tweepy.error.TweepError as e:
+        trends = json.loads(e.response.text)
+    return trends
+
+
 def get_tweets(query):
-        api=tweepy.API(auth)
-        try:
-                tweets = api.search(query)
-        except tweepy.error.TweepError as e:
-                tweets = [json.loads(e.response.text)]
-        return tweets
-tw = get_tweets("#HanSolo")
-
-queries = ["#HanSolo -filter:retweets lang:en", "\"Nova Scotia\" -filter:retweets lang:en","@Windows -filter:retweets lang:en", "#realDonaldTrump -filter:retweets lang:en"]
-
-with open ('tweet.csv', 'w') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerow(['id', 'user', 'created_at', 'text'])
-        for query in queries:
-                t = get_tweets(query)
-                for tweet in t:
+    api = tweepy.API(auth)
+    try:
+        tweets = api.search(query)
+    except tweepy.error.TweepError as e:
+        tweets = [json.loads(e.response.text)]
+    return tweets
 
 
-                        writer.writerow([(tweet.id_str),
-                                         (tweet.user.screen_name),
-                                         tweet.created_at,
-                                         (tweet.text.encode("utf-8"))])
+# tw = get_tweets("#HanSolo")
 
 
+queries = ["#HanSolo -filter:retweets lang:en", "\"Nova Scotia\" -filter:retweets lang:en",
+           "@Windows -filter:retweets lang:en", "#realDonaldTrump -filter:retweets lang:en",
+           "#iOS12 -filter:retweets lang:en", "#Mojave -filter:retweets lang:en", "#E3 -filter:retweets lang:en"]
 
-'''
-Code to clean the tweets for analysis
-'''
-list = []
+
+with open('tweet.csv', 'w') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(['id', 'user', 'created_at', 'text'])
+    for query in queries:
+        t = get_tweets(query)
+        for tweet in t:
+            writer.writerow([(tweet.id_str),
+                             (tweet.user.screen_name),
+                             tweet.created_at,
+                             (tweet.text.encode("utf-8"))])
+#
+# '''
+# Code to clean the tweets for analysis
+# '''
+#
+# df = pd.read_csv('tweet.csv')
+#
+# df_clean = df.drop_duplicates(subset=['text'])
+#
+# df_clean.to_csv('tweet.csv')
+# df.update(df_clean)
+# lists = []
+# for index, row in df_clean.iterrows():
+#     '''Code to remove Url, RTs and # tags were inspired from:
+#     1. https://stackoverflow.com/a/8377440/3966666
+#     2. https://knowledge.safe.com/questions/29604/regex-to-extract-url-from-tweet.html
+#     '''
+#
+#     text = row['text']
+#     text = re.sub("http[a-zA-Z]?:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?", '',text).strip()
+#     text = re.sub('RT @[\w_]+:', '', text)
+#     text = re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"', '', text)
+#     lists.append(text)
+#
+#
+# print(lists)
+#
+# for index, row in df_clean.iterrows():
+#
+#     row['text'] = lists[index]
+#
+#
+# df.update(df_clean)
+# df.to_csv('clean_tweet.csv')
+lists = []
 with open('clean_tweet.csv', 'w') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(['id', 'user', 'created_at', 'text'])
+        i = 0
         for query in queries:
                 t = get_tweets(query)
                 for tweet in t:
@@ -77,23 +121,60 @@ with open('clean_tweet.csv', 'w') as outfile:
 
                         text = re.sub(r"http[a-zA-Z]?:\/\/?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?", '',
                                       tweet.text).strip()
-                        text = re.sub('RT @[\w_]+:', '', text)
+                        # text = re.sub('RT @[\w_]+:', '', text)
                         text = re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"', '', text)
 
-                        list.append(text)
+                        lists.append(text)
+                        list(unique_everseen(lists))
                         writer.writerow([(tweet.id_str),
                                          (tweet.user.screen_name),
                                          tweet.created_at,
-                                         (text)])
+                                         lists[i]])
+                        i += 1
 
+
+
+'''
+Code to remove duplicates the tweets for analysis
+'''
+
+df = pd.read_csv('clean_tweet.csv')
+
+df_clean = df.drop_duplicates(subset=['text'])
+lists = df_clean['text'].tolist()
+df_clean.to_csv('clean_tweet.csv', index=False)
 
 
 analyzer = SentimentIntensityAnalyzer()
 
 
 vs = {}
-
-for sentence in list:
+sentimentList = []
+for sentence in lists:
     vs = analyzer.polarity_scores(sentence)
-    print(" {:-<65} {} ".format(sentence, str(vs)))
+    # print(" {:-<65} {} ".format(sentence, str(vs)))
+    sentimentList.append(max(vs.items(), key=operator.itemgetter(1))[0])
+    print (max(vs.items(), key=operator.itemgetter(1))[0])
+
+
+
+
+
+with open('sentiment_output.csv', 'w') as f2:
+    with open('clean_tweet.csv', 'r') as infile:
+        next(infile)
+        reader = csv.reader(infile)
+        writer = csv.writer(f2)
+        writer.writerow(['id', 'user', 'created_at', 'text', 'sentiment'])
+
+        i=0
+        for rows in reader:
+            rows.append(sentimentList[i])
+            writer.writerow(rows)
+            i += 1
+
+
+
+
+
 
